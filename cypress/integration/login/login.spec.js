f54389bf785128a52db86072855610c675d9acb7
Olabanj <olabanjolukman5@gmail.com>
2022-12-01 03:10:48 +0100

    add new feature

--- cypress/integration/login/login.spec.js
+++ cypress/integration/login/login.spec.js
@@ -1,6 +1,6 @@
 /// <reference types="Cypress" />
 import LoginPage from "../POM/LoginPage"
-
+const login = new LoginPage
 
 
 Cypress.on('uncaught:exception', (err, runnable) => {
@@ -15,11 +15,25 @@ Cypress.on('uncaught:exception', (err, runnable) => {
         })
         it("login functionalty",function(){
             
-            const login = new LoginPage();
+            
        cy.visit('/')
         login.typeUser().type(this.data.Username1)
         login.typePass().type(this.data.Password)
         login.loginSubmit().click()
+
+        //Add to carts
+        
+        login.clickBap().click()
+        login.clickBike().click()
+     
+        
+        // click on shopping cart link
+        login.clickShop().click()
+    
+        //continue shopping
+        login.contShop().click()
+
+
     })
 
 

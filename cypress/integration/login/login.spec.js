a1686f258cd9eac3d526faa98fa9156ea9d6ad92
Olabanj <olabanjolukman5@gmail.com>
2022-12-01 01:48:24 +0100

    cypress commit

---
+++ cypress/integration/login/login.spec.js
@@ -0,0 +1,21 @@
+/// <reference types="Cypress" />
+import LoginPage from '../POM/LoginPage'
+const loginPage = new LoginPage();
+
+    describe("login test",function(){
+        before(function() {
+            cy.fixture('loginData').then(function(data) {
+                this.data=data
+            })
+        })
+        it("login functionalty",function(){
+       
+      
+        cy.visit('/')
+        loginPage.typeUser().type(this.data.Username1)
+        loginPage.typePass().type(this.data.Password)
+        loginPage.loginSubmit().click()
+    })
+
+
+})
